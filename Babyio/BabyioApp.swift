d63c349f304803024b477e57b7afc3fed921eb7b
mfsaglam <mfsaglam1@gmail.com>
2021-10-07 13:09:25 +0300

    Initial Commit

---
+++ Babyio/BabyioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyioApp.swift
+//  Babyio
+//
+//  Created by Fatih Sağlam on 7.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct BabyioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
